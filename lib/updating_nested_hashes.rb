2d88ed230e8564d202f59b7ce3daadedbc6e5c27
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-04-24 19:33:33 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -51,10 +51,6 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-      :additional_characters => [
-       {name: "prince Escalus"},
-       {name: "Apothecary"}
-        ]
    }
 
    # Write code below that does the following programmatically:
@@ -64,6 +60,11 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
+
+epic_tragedy[:additional_characters] = []
+
+epic_tragedy[:additional_characters] << {name: "Prince Escalus"}
+epic_tragedy[:additional_characters] << {name: "Apothecary"}
 
 
 
